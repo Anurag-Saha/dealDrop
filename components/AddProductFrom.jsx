43f3d442b4b4b96e4ef5644e8bea0d39beacb75e
Anurag Saha <anuragsaha9183@gmail.com>
2025-12-16 11:41:20 +0530

    add product from ui implemented

--- components/AddProductFrom.jsx
+++ components/AddProductFrom.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
+"use client";
 
-const AddProductFrom = ({user}) => {
+import React, { useState } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
+
+const AddProductFrom = ({ user }) => {
+  const [url, setUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+  const handelSubmit = async (e) => {};
+
   return (
-    <form>
-      <div className='flex felx-col sm:flex-row gap-2'>
-        
-      </div>
-    </form>
-  )
-}
+    <>
+      <form onSubmit={handelSubmit} className="w-full max-w-2xl mx-auto">
+        <div className="flex felx-col sm:flex-row gap-2">
+          <Input
+            type="url"
+            value={url}
+            onchange={(e) => setUrl(e.target.value)}
+            placeholder="Enter product url (Amazon Walmart etc)"
+            className="h-12 text-base"
+            required
+            disabled={loading}
+          />
+          <Button
+            className="bg-orange-500 hover:bg-orange-600 h-10 sm:h-12 px-8"
+            type="submit"
+            disabled={loading}
+            size={"lg"}
+          >
+            {loading ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Adding..
+              </>
+            ) : (
+              "Track Price"
+            )}
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+};
 
-export default AddProductFrom
+export default AddProductFrom;
